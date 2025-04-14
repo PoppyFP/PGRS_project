fc361c306a68a37946b6d617383f5ebd6a8f9922
PoppyFP <Forshaw_Perring-P@ulster.ac.uk>
2025-04-14 12:07:12 +0100

    Update AQ_data_process.py
    Spatial joins of nitrogen/acid data and protected area polygons, and create figure/axes and print SAC data to figure

--- AQ_data_process.py
+++ AQ_data_process.py
@@ -2,6 +2,9 @@
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
+import matplotlib.pyplot as plt
+from cartopy.feature import ShapelyFeature
+import cartopy.crs as ccrs
 
 #Add protected areas polygon datasets
 SAC_data = gpd.read_file('NE_data/Special_Area_of_Conservation.gpkg')
@@ -14,12 +17,12 @@ df = pd.read_csv('AQ_data.csv')
 #Uncomment below to show the CSV data
 #print(df)
 
-#Create points from the XY data in the AQ CSV
-df['geometry'] = list(zip(df['X'], df['Y']))
-df['geometry'] = df['geometry'].apply(Point)
-
 #Uncomment below to show the CSV data, which should now have an added 'geometry' column with POINT (X, Y) in for each row
 #print(df)
+
+#Create geometry from the XY data in the AQ CSV
+df['geometry'] = list(zip(df['X'], df['Y']))
+df['geometry'] = df['geometry'].apply(Point)
 
 #Uncomment below to delete the separate 'X' and 'Y' columns, as they are now extraneous
 #del df['X'], df['Y']
@@ -44,7 +47,45 @@ gdf_nit_select = gdf_nit_select.set_crs("EPSG:27700")
 gdf_acid_select = gdf_acid_select.set_crs("EPSG:27700")
 
 #Save GeoDataFrame to geopackage
-gdf_nit_select.to_file('AQ_points_nit_0.1.gpkg')
-gdf_acid_select.to_file('AQ_points_acid_0.1.gpkg')
+#gdf_nit_select.to_file('AQ_points_nit_0.1.gpkg')
+#gdf_acid_select.to_file('AQ_points_acid_0.1.gpkg')
 
-#Next step: spatial join
+#Create list of protected areas for use in loop?
+#list = [SAC_data, SPA_data, SSSI_data]
+#for item in list:
+    #join_list = gpd.sjoin(gdf_nit_select, )
+
+#Or join one at a time
+join_SAC_nit = gpd.sjoin(SAC_data, gdf_nit_select, how='inner', lsuffix='left', rsuffix='right')
+#print(join_SAC_nit)
+#SAC_nit = gpd.GeoDataFrame(join_SAC_nit)
+
+join_SPA_nit = gpd.sjoin(SPA_data, gdf_nit_select, how='inner', lsuffix='left', rsuffix='right')
+#print(join_SPA_nit)
+
+join_SSSI_nit = gpd.sjoin(SSSI_data, gdf_nit_select, how='inner', lsuffix='left', rsuffix='right')
+#print(join_SSSI_nit)
+
+join_SAC_acid = gpd.sjoin(SAC_data, gdf_acid_select, how='inner', lsuffix='left', rsuffix='right')
+#print(join_SAC_acid)
+
+join_SPA_acid = gpd.sjoin(SPA_data, gdf_acid_select, how='inner', lsuffix='left', rsuffix='right')
+#print(join_SPA_acid)
+
+join_SSSI_acid = gpd.sjoin(SSSI_data, gdf_acid_select, how='inner', lsuffix='left', rsuffix='right')
+#print(join_SSSI_acid)
+
+#Create figures with the joined data
+uk_utm = ccrs.UTM(30)
+ccrs.CRS(SAC_data.crs)
+
+fig = plt.figure(figsize=(10, 10))
+ax = plt.axes(projection=uk_utm)
+
+SAC_feature = ShapelyFeature(SAC_data['geometry'], uk_utm, edgecolor='g', facecolor='g')
+ax.add_feature(SAC_feature)
+
+xmin, ymin, xmax, ymax = SAC_data.total_bounds # using the boundary of the shapefile features, zoom the map to our area of interest
+ax.set_extent([xmin-3000, xmax+3000, ymin-3000, ymax+3000], crs=uk_utm)
+
+fig.savefig('SAC.png', bbox_inches='tight', dpi=300)
