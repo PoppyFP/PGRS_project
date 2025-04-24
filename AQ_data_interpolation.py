83d218851c3628ed2e2917f198dc9039941bd1f3
PoppyFP <Forshaw_Perring-P@ulster.ac.uk>
2025-04-24 20:37:12 +0100

    Point interpolation
    Interpolation using pykrige

--- AQ_data_interpolation.py
+++ AQ_data_interpolation.py
@@ -1,4 +1,6 @@
 #imports
+from cProfile import label
+
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
@@ -6,6 +8,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
 from pykrige.ok import OrdinaryKriging
+import cartopy.crs as ccrs
 import rasterio
 import rasterio.mask
 from rasterio.plot import show
@@ -31,6 +34,7 @@ AQ_value = AQ_points.iloc[:, 3].tolist()
 
 min_x_AQ, min_y_AQ, max_x_AQ, max_y_AQ = AQ_points.total_bounds
 
+#Run Ordinary Kriging using the PyKrige module
 OK = OrdinaryKriging(
     np.array(x_AQ),
     np.array(y_AQ),
@@ -41,3 +45,18 @@ OK = OrdinaryKriging(
     coordinates_type = "euclidean",
 )
 
+uk_utm = ccrs.UTM(30)
+
+#Create figure for graph of interpolated points
+fig = plt.figure(figsize = (10, 10))
+ax = plt.axes(projection=uk_utm)
+
+#Plot data
+AQ_points.plot(ax = ax, marker = 'o', color = 'royalblue', markersize = 3, label = 'AQ_value')
+
+for ind, row in AQ_points.iterrows():
+    x, y = row.geometry.x, row.geometry.y
+    ax.text(x, y, row['AQ_value'].title(), fontsize=7, transform=ccrs.PlateCarree())
+
+#Save figure
+fig.savefig('Interpolation.png', bbox_inches='tight', dpi=300)
