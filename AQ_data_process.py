9c9a64543df57986b122ab1fc69032f50ec874ca
PoppyFP <Forshaw_Perring-P@ulster.ac.uk>
2025-04-28 12:20:11 +0100

    Update AQ_data_process.py
    Defining first function for clipping data

--- AQ_data_process.py
+++ AQ_data_process.py
@@ -10,13 +10,23 @@ from cartopy.feature import ShapelyFeature
 SAC_data = gpd.read_file('NE_data/Special_Area_of_Conservation.gpkg')
 AQ_points = gpd.read_file("AQ_points.shp")
 
-SAC_AQ = gpd.clip(AQ_points, SAC_data)
+def AQ_clip (A, B, output_path):
 
-#SAC_AQ.to_file('SAC_AQ.shp')
+    """Clip air quality point data by protected area polygon data
+    Args:
+        A = point data to be clipped
+        B = polygon data e.g. from natural england, Special Areas of Conservation
+        
+    Returns: a GeoDataFrame of points within A which are within the selected polygon (B)"""
+
+    aq_clipped = gpd.clip(A, B)
+    aq_clipped.to_file(output_path)
+    return AQ_clip
+
+AQ_clip(AQ_points, SAC_data, 'SAC_AQ.shp')
 
 SAC_AQ_points = gpd.read_file('SAC_AQ.shp')
 SAC_AQ_points.crs
-print(SAC_AQ_points.crs)
 
 uk_utm = ccrs.UTM(30)
 
@@ -27,8 +37,6 @@ SAC_feature = ShapelyFeature(SAC_data['geometry'], uk_utm, edgecolor='g', faceco
 ax.add_feature(SAC_feature)
 
 SAC_AQ_plot = ax.plot(SAC_AQ_points.X, SAC_AQ_points.Y, 's', color='r', ms=2, markerfacecolor='r', transform=ccrs.UTM(30), zorder=2)
-
-#SAC_AQ.plot(ax = ax, marker = 'o', color = 'royalblue', markersize = 3)
 
 xmin, ymin, xmax, ymax = SAC_AQ_points.total_bounds
 ax.set_extent([xmin-2000, xmax+2000, ymin-2000, ymax+2000], crs=uk_utm)
