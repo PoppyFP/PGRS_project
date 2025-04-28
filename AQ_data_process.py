4ea37483eeb83d09b1a1ebd8ad705a275a804a7a
PoppyFP <Forshaw_Perring-P@ulster.ac.uk>
2025-04-28 20:55:23 +0100

    Update AQ_data_process.py
    Adding north arrow to figure

--- AQ_data_process.py
+++ AQ_data_process.py
@@ -15,8 +15,8 @@ SPA_data_full = gpd.read_file('NE_data/Special_Protection_Areas.gpkg')
 SSSI_data_full = gpd.read_file('NE_data/Sites_Special_Scientific_Interest.gpkg')
 AQ_points = gpd.read_file("AQ_points.shp")
 
-Select_AQ_points = AQ_points[AQ_points['Grid_data_Nit_dep'] > 0.1]
-print(Select_AQ_points)
+#Select_AQ_points = AQ_points[AQ_points['Grid_data_'] > 0.1]
+#print(Select_AQ_points)
 
 #Clip Natural England data to the site
 
@@ -70,8 +70,8 @@ ax = plt.axes(projection=uk_utm)
 Outline_feature = ShapelyFeature(Data_outline['geometry'], uk_utm, edgecolor='k', facecolor='w', zorder=0)
 ax.add_feature(Outline_feature)
 
-SAC_feature = ShapelyFeature(SAC_data['geometry'], uk_utm, edgecolor='g', facecolor='g', alpha = 0.5, zorder=1)
-ax.add_feature(SAC_feature)
+NE_feature = ShapelyFeature(SAC_data['geometry'], uk_utm, edgecolor='g', facecolor='g', alpha = 0.5, zorder=1)
+ax.add_feature(NE_feature)
 
 AQ_plot = ax.plot(AQ_clip_points.X, AQ_clip_points.Y, 'o', color='b', ms=2, markerfacecolor='b', transform=ccrs.UTM(30), zorder=2)
 
@@ -79,45 +79,51 @@ xmin, ymin, xmax, ymax = AQ_clip_points.total_bounds
 ax.set_extent([xmin-1200, xmax+1200, ymin-1200, ymax+1200], crs=uk_utm)
 
 gridlines = ax.gridlines(draw_labels=True,
-                        # xlocs=[5, 10, 15, 20, 25, 30],
-                        # ylocs=[54, 54.5, 55, 55.5])
+                         xlocs=[5, 10, 15, 20, 25, 30],
+                         ylocs=[54, 54.5, 55, 55.5])
 
-#ax.add_artist(ScaleBar(distance_meters))
+#Add north arrow (adapted from https://stackoverflow.com/questions/58088841/how-to-add-a-north-arrow-on-a-geopandas-map)
 
-# def scale_bar(ax, length=5, location=(0.92, 0.95)):
-#     """
-#     Create a scale bar in a cartopy GeoAxes.
-#
-#     Parameters:
-#
-#     ax : cartopy.mpl.geoaxes.GeoAxes
-#         the cartopy GeoAxes to add the scalebar to.
-#
-#     length : int, float (default 20)
-#         the length of the scalebar, in km
-#
-#     location : tuple(float, float) (default (0.92, 0.95))
-#         the location of the center right corner of the scalebar, in fractions of the axis.
-#
-#     Returns:
-#
-#     ax : cartopy.mpl.geoaxes.GeoAxes
-#         the cartopy GeoAxes object
-#
-#     """
-#     x0, x1, y0, y1 = ax.get_extent() # get the current extent of the axis
-#     sbx = x0 + (x1 - x0) * location[0] # get the right x coordinate of the scale bar
-#     sby = y0 + (y1 - y0) * location[1] # get the right y coordinate of the scale bar
-#
-#     ax.plot([sbx, sbx-length*500], [sby, sby], color='k', linewidth=4, transform=ax.projection) # plot a thick black line
-#     ax.plot([sbx-(length/2)*500, sbx-length*500], [sby, sby], color='w', linewidth=2, transform=ax.projection) # plot a white line from 0 to halfway
-#
-#     ax.text(sbx, sby-(length/4)*100, f"{length} km", ha='center', transform=ax.projection, fontsize=6) # add a label at the right side
-#     ax.text(sbx-(length/2)*500, sby-(length/4)*100, f"{int(length/2)} km", ha='center', transform=ax.projection, fontsize=6) # add a label in the center
-#     ax.text(sbx-length*500, sby-(length/4)*100, '0 km', ha='center', transform=ax.projection, fontsize=6) # add a label at the left side
-#
-#     return ax
-#
-# scale_bar(ax)
+x, y, arrow_length = 0.05, 0.97, 0.1
+ax.annotate('N', xy=(x, y), xytext=(x, y-arrow_length),
+            arrowprops=dict(facecolor='black', width=5, headwidth=15),
+            ha='center', va='center', fontsize=20,
+            xycoords=ax.transAxes)
 
-#fig.savefig('clipped_AQ.png', bbox_inches='tight', dpi=300)
+def scale_bar(ax, length=5, location=(0.92, 0.95)):
+    """
+    Create a scale bar in a cartopy GeoAxes.
+
+    Parameters:
+
+    ax : cartopy.mpl.geoaxes.GeoAxes
+        the cartopy GeoAxes to add the scalebar to.
+
+    length : int, float (default 20)
+        the length of the scalebar, in km
+
+    location : tuple(float, float) (default (0.92, 0.95))
+        the location of the center right corner of the scalebar, in fractions of the axis.
+
+    Returns:
+
+    ax : cartopy.mpl.geoaxes.GeoAxes
+        the cartopy GeoAxes object
+
+    """
+    x0, x1, y0, y1 = ax.get_extent() # get the current extent of the axis
+    sbx = x0 + (x1 - x0) * location[0] # get the right x coordinate of the scale bar
+    sby = y0 + (y1 - y0) * location[1] # get the right y coordinate of the scale bar
+
+    ax.plot([sbx, sbx-length*500], [sby, sby], color='k', linewidth=4, transform=ax.projection) # plot a thick black line
+    ax.plot([sbx-(length/2)*500, sbx-length*500], [sby, sby], color='w', linewidth=2, transform=ax.projection) # plot a white line from 0 to halfway
+
+    ax.text(sbx, sby-(length/4)*200, f"{length} km", ha='center', transform=ax.projection, fontsize=6) # add a label at the right side
+    ax.text(sbx-(length/2)*500, sby-(length/4)*200, f"{int(length/2)} km", ha='center', transform=ax.projection, fontsize=6) # add a label in the center
+    ax.text(sbx-length*500, sby-(length/4)*200, '0 km', ha='center', transform=ax.projection, fontsize=6) # add a label at the left side
+
+    return ax
+
+scale_bar(ax)
+
+fig.savefig('clipped_AQ.png', bbox_inches='tight', dpi=300)
